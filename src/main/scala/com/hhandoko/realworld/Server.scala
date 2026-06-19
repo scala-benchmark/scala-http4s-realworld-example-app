5f8d088e503e10326fd17a176eca5ac1532d4eff
Bortolla <vitorlacobortolotti@gmail.com>
2026-06-19 10:54:25 -0300

    Fix: sink lines

--- src/main/scala/com/hhandoko/realworld/Server.scala
+++ src/main/scala/com/hhandoko/realworld/Server.scala
@@ -2,11 +2,14 @@ package com.hhandoko.realworld
 
 import java.util.Base64
 import scala.concurrent.ExecutionContext
+
 import cats.effect.{Async, Blocker, ConcurrentEffect, ContextShift, Resource, Sync, Timer}
 import cats.effect.concurrent.Ref
 import cats.implicits._
+
 import doobie.hikari.HikariTransactor
 import doobie.util.ExecutionContexts
+
 import org.http4s.HttpRoutes
 import org.http4s.MediaType
 import org.http4s.dsl.Http4sDsl
@@ -14,10 +17,13 @@ import org.http4s.headers.`Content-Type`
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.server.middleware.Logger
 import org.http4s.server.{Router, Server => BlazeServer}
+
 import pureconfig.module.catseffect.loadConfigF
 import com.hhandoko.realworld.auth.RequestAuthenticator
 import com.hhandoko.realworld.config.{Config, DbConfig, LogConfig, ServerConfig}
+
 import slick.jdbc.JdbcBackend.Database
+
 import com.hhandoko.realworld.repository.{ArticleRepo, AssetDirectoryRequest, EvalRequest, UserRepo}
 import com.hhandoko.realworld.route.{ArticleRoutes, AuthRoutes, ProfileRoutes, TagRoutes, UserRoutes}
 import com.hhandoko.realworld.service.{ArticleService, AuthService, CommandService, FileService, HtmlService, LdapService, ProfileService, RedirectService, SqlService, TagService, UserService}
